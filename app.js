ffbc91d8168a2e44d40cd3e4f5f586d6e3969d48
Magnus Jonsson <magnus.dronar@gmail.com>
2017-01-25 13:31:24 +0100

    Create basic boilerplate

---
+++ app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// Dependencies
+
+const koa = require('koa');
+const Router = require('koa-router');
+const Pug = require('koa-pug');
+
+// Controllers
+
+const homeController = require('./controllers/home.js');
+
+// Set up application
+
+const isDebug = process.env === 'development';
+
+const app = koa();
+const router = new Router();
+
+const pug = new Pug({
+  viewPath: __dirname + '/views',
+  debug: isDebug,
+  noCache: isDebug,
+});
+pug.use(app);
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+// Error handling
+
+app.use(function *(next) {
+  try {
+    yield next;
+  } catch (err) {
+    this.type = 'json';
+    this.status = err.status || 500;
+    this.body = {error: '500 Error :('};
+    this.app.emit('error', err, this);
+  }
+});
+
+// Routing
+
+router.get('/', homeController.index);
+
+// Start app
+
+app.listen(3000);
+console.log('Application started at http://localhost:3000');
---
+++ controllers/home.js
@@ -0,0 +1,5 @@
+'use strict';
+
+exports.index = function *(next) {
+  this.render('home/index.pug');
+};
